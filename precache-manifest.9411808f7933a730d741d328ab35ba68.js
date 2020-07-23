6d044071c4bcc473cfadcf3ef055bc229124695f
Gonzalo Neira <ggneiracc68@gmail.com>
2020-07-23 01:29:47 -0500

    Updates

---
+++ precache-manifest.9411808f7933a730d741d328ab35ba68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e16d0e89612321852337fd16e77860",
+    "url": "/ggneiracc68/sesion09/index.html"
+  },
+  {
+    "revision": "13660b9300f5cff7ae3c",
+    "url": "/ggneiracc68/sesion09/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f97528078f08d8689be1",
+    "url": "/ggneiracc68/sesion09/static/js/2.2ebf5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ggneiracc68/sesion09/static/js/2.2ebf5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13660b9300f5cff7ae3c",
+    "url": "/ggneiracc68/sesion09/static/js/main.8df798e7.chunk.js"
+  },
+  {
+    "revision": "82260e30110c337a9e4f",
+    "url": "/ggneiracc68/sesion09/static/js/runtime-main.f173f116.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ggneiracc68/sesion09/static/media/logo.5d5d9eef.svg"
+  }
+]);
